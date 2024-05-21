b9852b614a7eb22e40b91d2cba0356435b6bacc8
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-21 17:31:06 +0900

    Title: 날짜 변경하여 출력 2, Time: 175ms, Memory: 15MB, Status: Passed - CodeTree

--- 240521/날짜 변경하여 출력 2/print-date-with-different-format-2.js
+++ 240521/날짜 변경하여 출력 2/print-date-with-different-format-2.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 let date = fs.readFileSync(0).toString().split("-");
 
 let yyyy = date[2];
-let mm = date[1];
-let dd = date[0];
+let mm = date[0];
+let dd = date[1];
 
 console.log(`${yyyy}.${mm}.${dd}`);
