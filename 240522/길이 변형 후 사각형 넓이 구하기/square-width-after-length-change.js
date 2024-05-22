4db92d42995b5b162ddc0324af70a8f7961f2f92
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-22 23:44:24 +0900

    Title: 길이 변형 후 사각형 넓이 구하기, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240522/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.js
+++ 240522/길이 변형 후 사각형 넓이 구하기/square-width-after-length-change.js
@@ -4,12 +4,9 @@ let input = fs.readFileSync(0).toString().split(" ");
 let length = Number(input[0]);
 let hight = Number(input[1]);
 
-
 length += 8;
 hight *= 3;
 
-let area = length * hight;
-
 console.log(length);
 console.log(hight);
-console.log(area);
+console.log(length * hight);
