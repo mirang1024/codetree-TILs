34a114f1f1432efe2bcfac9e8e089faac8b2869a
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-23 16:14:26 +0900

    Title: 세 수의 합과 평균 구하기, Time: 181ms, Memory: 14MB, Status: Passed - CodeTree

--- 240523/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
+++ 240523/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -6,7 +6,7 @@ let b = Number(input[1]);
 let c = Number(input[2]);
 
 let sum = a + b + c;
-let avr = (sum / input.length ).toFixed(0);
+let avr = Math.floor(sum / input.length);
 
 console.log(sum);
 console.log(avr);
