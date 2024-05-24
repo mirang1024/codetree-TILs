a9524a1442a4d646871eabdb909b7059cc7a072b
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-25 00:12:24 +0900

    Title: 큰 수에서 빼기, Time: 177ms, Memory: 14MB, Status: Passed - CodeTree

--- 240525/큰 수에서 빼기/subtract-from-large-number.js
+++ 240525/큰 수에서 빼기/subtract-from-large-number.js
@@ -2,15 +2,10 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().split(" ");
 
 let a = Number(input[0]);
-if (b === null) {
-    let b = 0
-} else {
-    let b = Number(input[1]);
-}
+let b = Number(input[1]);
 
-
-if (a > b) {
+if (a >= b) {
     console.log(a - b);
-} else if (b > a) {
+} else if (b >= a) {
     console.log(b - a);
 }
