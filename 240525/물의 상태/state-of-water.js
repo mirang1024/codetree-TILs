cfb76387755fa63b9f5fe5dd8116deefe345aaf5
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-25 18:47:57 +0900

    Title: 물의 상태, Time: 222ms, Memory: 15MB, Status: Passed - CodeTree

--- 240525/물의 상태/state-of-water.js
+++ 240525/물의 상태/state-of-water.js
@@ -1,16 +1,12 @@
 const fs = require("fs");
 let n = Number(fs.readFileSync(0).toString().trim());
 
-
-
-
 if (n >= 100) {
     console.log("vapor");
 }
 
-if (n >= 0) {
-    console.log("water");
-}
-else if (n < 0) {
+if (n < 0) {
     console.log("ice")
+} else if (n < 100) {
+    console.log("water");
 }
