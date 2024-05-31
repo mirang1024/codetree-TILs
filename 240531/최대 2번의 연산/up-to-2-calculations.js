fe78372a8b17b9edd2fde7bbdb357f321a288007
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-05-31 15:29:55 +0900

    Title: 최대 2번의 연산, Time: 168ms, Memory: 15MB, Status: Wrong Answer - CodeTree

---
+++ 240531/최대 2번의 연산/up-to-2-calculations.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+let a = Number(fs.readFileSync(0).toString().trim());
+
+if (a % 2 === 0) {
+}
+let result = a / 2
+
+if (result % 2 === 1) {
+    let final = (result + 1) / 2
+    console.log(final);
+}
