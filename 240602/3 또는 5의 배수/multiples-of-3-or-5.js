61deac6731fe78949ea20de4ade3609d17b3831c
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-06-02 00:42:44 +0900

    Title: 3 또는 5의 배수, Time: 166ms, Memory: 14MB, Status: Passed - CodeTree

--- 240602/3 또는 5의 배수/multiples-of-3-or-5.js
+++ 240602/3 또는 5의 배수/multiples-of-3-or-5.js
@@ -2,15 +2,15 @@ const fs = require("fs");
 const a = Number(fs.readFileSync(0).toString().trim());
 
 if (a % 3 === 0) {
-    console.log("Yes");
+    console.log("YES");
 }
 else {
-    console.log("No");
+    console.log("NO");
 }
 
 if (a % 5 === 0) {
-    console.log("Yes");
+    console.log("YES");
 }
 else {
-    console.log("No");
+    console.log("NO");
 }
