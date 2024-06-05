6f0c3b7233315dd54e1b8946931f1e249468a5ba
Mirang Choi <158833693+mirang1024@users.noreply.github.com>
2024-06-05 14:10:39 +0900

    Title: 4번의 크기 비교, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240605/4번의 크기 비교/4-time-comparison.js
+++ 240605/4번의 크기 비교/4-time-comparison.js
@@ -2,7 +2,9 @@ const fs = require("fs");
 let input = fs.readFileSync(0).toString().trim().split("\n");
 
 const a = Number(input[0]);
+
 const input2 = input[1].split(" ");
+
 const b = Number(input2[0]);
 const c = Number(input2[1]);
 const d = Number(input2[2]);
@@ -20,13 +22,13 @@ if (a > c) {
     console.log(0);
 }
 
-if (b > d) {
+if (a > d) {
     console.log(1);
 } else {
     console.log(0);
 }
 
-if (b > e) {
+if (a > e) {
     console.log(1);
 } else {
     console.log(0);
